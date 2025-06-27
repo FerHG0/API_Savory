6c869496dac778ae5646e6bba3ba5f9f1d05daa0
FerHG0 <2236001342@utna.edu.mx>
2025-06-26 19:35:06 -0600

    Conexion desde moongose a mongodb

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./app/app')
 const config = require('./app/config/config')
+const conexion = require('./app/config/conexion')
+
+conexion.connect()
 
 app.listen(config.PORT,()=> {
     console.log(`Aplicacin corriendo en el puerto ${config.PORT}`);
