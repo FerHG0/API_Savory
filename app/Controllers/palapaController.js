281911293903697f4b15979e668b2ff085842a4e
FerHG0 <2236001342@utna.edu.mx>
2025-07-22 19:12:25 -0600

    API REST completa (Consultar, Crear, Eliminar y Actualizar)

--- app/Controllers/palapaController.js
+++ app/Controllers/palapaController.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const palapaModel = require('../models/palapaModel')
 
 function buscarTodo(req, res){
@@ -18,7 +19,6 @@ function buscarTodo(req, res){
 }
 
 function agregar(req, res) {
-    //console.log(req.body)
     new palapaModel(req.body).save()
     .then(info => {
         return res.status(200).send({
@@ -33,7 +33,91 @@ function agregar(req, res) {
     })
 }
 
+function buscarBebida(req, res, next) {
+    if(!req.body)req.body={}
+    let consulta = {}
+    consulta[req.params.key]=req.params.value
+    console.log (consulta)
+    palapaModel.find(consulta)
+    .then(bebidas => {
+        if (!bebidas.length) return next
+        req.body.bebidas = bebidas
+        return next()
+    })
+    .catch (e => {
+        req.body.e = e
+        return next()
+    })
+}
+
+function mostrarBebida(req, res) {
+    let bebidas = req.body.bebidas
+
+    if (req.body.e) return res.status(404).send({
+        message: "Error al consultar la informacion"
+    })
+
+    if (!bebidas) return res.status(204).send({
+        message: "No hay informacion que mostrar"
+    })
+    
+    return res.status(200).send({bebidas})
+}
+
+function eliminarBebida(req, res) {
+    var bebidas = {}
+    bebidas= req.body.bebidas
+    palapaModel.deleteOne(bebidas[0])
+    .then(info => {
+        return res.status(200).send({
+            message: "bebida eliminada con exito"
+        })
+    })
+    .catch(e => {
+        return res.status(404).send({
+            message: `error al eliminar la bebida ${e}`
+        })
+    })
+}
+
+function actualizarBebida(req, res) {
+    const bebidas = req.body.bebidas;
+    const nuevaData = req.body;
+
+    if (!bebidas || !bebidas.length) {
+        return res.status(404).send({
+            message: "No se encontró la bebida para actualizar"
+        });
+    }
+
+    palapaModel.updateOne(
+        { nombre: bebidas[0].nombre },  
+        nuevaData                      
+    )
+    .then(info => {
+        if (info.modifiedCount === 0) {
+            return res.status(200).send({
+                message: "No se realizaron cambios (ya estaba actualizado)"
+            });
+        }
+
+        return res.status(200).send({
+            message: "Bebida actualizada con éxito", info
+        });
+    })
+    .catch(e => {
+        return res.status(404).send({
+            message: `Error al actualizar la bebida: ${e}`
+        });
+    });    
+}
+
+
 module.exports = {
+    actualizarBebida,
     buscarTodo,
-    agregar
+    eliminarBebida,
+    agregar,
+    buscarBebida,
+    mostrarBebida
 }
